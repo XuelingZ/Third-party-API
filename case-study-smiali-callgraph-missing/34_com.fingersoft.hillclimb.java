9756bcf436530b8d02486e020d01021448fcde60
Xueling Zhang <30911899+XuelingZ@users.noreply.github.com>
2018-11-05 17:35:04 -0600

    Update 34_com.fingersoft.hillclimb.java

--- case-study-smiali-callgraph-missing/34_com.fingersoft.hillclimb.java
+++ case-study-smiali-callgraph-missing/34_com.fingersoft.hillclimb.java
@@ -1,1 +1,30 @@
-fddf
+package com.fingersoft.game.firebase;
+class C16891 implements ServiceConnection {
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        int i = VERSION.SDK_INT;
+        Log.m3487d(TAG, "onCreate");
+        mActivityInstance = this;
+        mFirebase = new Firebase(this);
+        startAnalytics(null);                                // caller , null
+      
+      
+    public static synchronized void startAnalytics(String str) {
+        synchronized (MainActivity.class) {
+            String str2 = TAG;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Start analytics with uid: ");
+            stringBuilder.append(str);
+            Log.m3487d(str2, stringBuilder.toString());
+            try {
+                mAnalyticsUid = str;
+                Log.m3487d(TAG, "Starting firebase analytics");
+                mFirebase.initializeAnalyticsWithUserId(str, new C23939());
+                mFirebase.getAnalyticsInstance().setUserID(str);                       // invoke
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return;
+    }
+      
