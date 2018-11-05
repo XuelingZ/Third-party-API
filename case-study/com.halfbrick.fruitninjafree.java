6d6b0a94dd5b08021e23922d329817373bfd59f0
Xueling Zhang <30911899+XuelingZ@users.noreply.github.com>
2018-11-05 14:09:36 -0600

    Create com.halfbrick.fruitninjafree.java

---
+++ case-study/com.halfbrick.fruitninjafree.java
@@ -0,0 +1,34 @@
+package com.halfbrick.mortar;
+public class Provider_AppsFlyerBackend {
+    private static Activity s_activity;
+    
+    
+    public static void onCreate() {
+        String GetAppKey = GetAppKey();
+        String GetDeviceID = GetDeviceID();
+        if (GetAppKey == null || GetAppKey.equals("")) {
+            Log.e("halfbrick.Mortar.AppsFlyerBackend", "onCreate> AppKey not found.");
+        } else if (GetDeviceID == null || GetDeviceID.equals("")) {
+            Log.e("halfbrick.Mortar.AppsFlyerBackend", "onCreate> DeviceID not found.");
+        } else {
+            Log.d("halfbrick.Mortar.AppsFlyerBackend", "onCreate> Initializing...");
+            StartTracking(GetAppKey, GetDeviceID);
+        }
+    }
+    
+    
+    
+    private static void StartTracking(String str, String str2) {
+        try {
+            Log.d("halfbrick.Mortar.AppsFlyerBackend", "StartTracking> DeviceID  : " + str2);
+            Log.d("halfbrick.Mortar.AppsFlyerBackend", "StartTracking> appKey    : " + str);
+            AppsFlyerLib.getInstance().setCustomerUserId(str2);
+            AppsFlyerLib.getInstance().startTracking(s_activity.getApplication(), str);
+        } catch (Throwable th) {
+            Log.e("halfbrick.Mortar.AppsFlyerBackend", "StartTracking> Failed to initialise AppsFlyer SDK");
+            Log.e("halfbrick.Mortar.AppsFlyerBackend", "StartTracking> AppsFlyer appKey: " + str);
+            Log.e("halfbrick.Mortar.AppsFlyerBackend", "" + th.getClass() + " thrown: " + th);
+        }
+    }
+    
+    ===============================================
