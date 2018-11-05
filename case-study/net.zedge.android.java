36760418a9b802c59f1b9f95e4a2a3705630241d
Xueling Zhang <30911899+XuelingZ@users.noreply.github.com>
2018-11-05 12:22:53 -0600

    Update net.zedge.android.java

--- case-study/net.zedge.android.java
+++ case-study/net.zedge.android.java
@@ -1,3 +1,29 @@
+package com.google.firebase.auth;
+public class FirebaseAuth implements zzeku {
+    public interface C1457a {
+        void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth);
+    }
+    
+    
+    
+package p000;    
+public final class dag implements Runnable {
+    /* renamed from: a */
+    private /* synthetic */ FirebaseAuth f20474a;
+
+    public dag(FirebaseAuth firebaseAuth) {
+        this.f20474a = firebaseAuth;
+    }
+
+    public final void run() {
+        for (C1457a onAuthStateChanged : this.f20474a.f12021e) {
+            onAuthStateChanged.onAuthStateChanged(this.f20474a);
+        }
+    }
+}
+    
+
+package net.zedge.android.log;    
 public final class MarketplaceLogger$initLogger$1 implements C1457a {
     final /* synthetic */ Context $context;
     final /* synthetic */ FirebaseAuth $firebaseAuth;
@@ -7,8 +33,7 @@ public final class MarketplaceLogger$initLogger$1 implements C1457a {
         this.$firebaseAuth = firebaseAuth;
     }
 
-
-public final void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+    public final void onAuthStateChanged(FirebaseAuth firebaseAuth) {
         eig.m21366b(firebaseAuth, "auth");
         daa c = firebaseAuth.mo17420c();
         if (c != null) {
@@ -16,3 +41,31 @@ public final void onAuthStateChanged(FirebaseAuth firebaseAuth) {
             this.$firebaseAuth.mo17418b((C1457a) this);
         }
     }
+}
+
+
+    
+    
+    
+package net.zedge.android.api.marketplace;
+public final class MarketplaceServiceRetrofitWrapper implements C1457a, MarketplaceService {
+ public final void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+        eig.m21366b(firebaseAuth, "auth");
+        daa c = firebaseAuth.mo17420c();
+        CharSequence c2 = c != null ? c.mo19447c() : null;
+        Object obj = (c2 == null || c2.length() == 0) ? 1 : null;
+        if (obj == null) {
+            Object c3 = firebaseAuth.mo17420c();
+            if (c3 == null) {
+                eig.m21362a();
+            }
+            eig.m21363a(c3, "auth.currentUser!!");
+            c3 = c3.mo19447c();
+            eig.m21363a(c3, "auth.currentUser!!.uid");
+            this.uid = c3;
+            if (this.balance == -1) {
+                getUserDataInternal(null);
+            }
+        }
+    }
+ }
