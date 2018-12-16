d5299bc2102fe4a3265bf48e65f1734833259b16
Xueling Zhang <30911899+XuelingZ@users.noreply.github.com>
2018-12-16 16:44:25 -0600

    Add files via upload

--- invokeDetection/install.py
+++ invokeDetection/install.py
@@ -1,27 +1,33 @@
-#coding=utf-8
-#adb install Babe.apk
-#aapt dump badging Babe.apk  | findstr "package"
+# install apk (10 app each batch) and start the app on device
+
+
 import os
 import time
 apkPath = "/home/xueling/apkAnalysis/invokeDetection/apks/"
 platformPath = "/home/xueling/Android/Sdk/platform-tools"
+packageNameListALL = []
+
+
+def uninstallApk(packageName):
+    cmd="adb uninstall %s"%packageName
+    print getCmdEexcuteResult(cmd)
+
+#
 
 def getCmdEexcuteResult(cmd):
     tmp = os.popen(cmd).readlines()
+    # print "command result : "
+    # print tmp
     return tmp
 
-def getPackageName(installName):
-    cmd='aapt dump badging "%s"  | findstr "package"'%installName
 
+def getPackageName(installName):
+    installName = apkPath + installName
+    # cmd='aapt dump badging "%s"  | findstr "package"'%installName
+    cmd = 'aapt dump badging "%s" '%installName
     print cmd
     str= getCmdEexcuteResult(cmd)[0].split(" ")[1]
     return str[6:-1]
-
-def getPackageNameInfo(packageName):
-    cmd='adb shell dumpsys package %s'%packageName
-    print cmd
-    exeResutl= getCmdEexcuteResult(cmd)[3]
-    return exeResutl
 
 def getPackageNameInfo(packageName):
     cmd='adb shell dumpsys package %s'%packageName
@@ -36,6 +42,7 @@ def getPackageNameInfo(packageName):
 def startApk(packageName):
     try:
         packagename_mainActivity = getPackageNameInfo(packageName)
+        print packagename_mainActivity
     except:
         print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         packagename_mainActivity = packageName + "/"
@@ -49,86 +56,65 @@ def installApk(name):
     name = apkPath + name
     cmd='./adb  install "%s"' %name
     os.chdir(platformPath)
-    os.system(cmd)
+    return os.system(cmd)
 
-def uninstallApk(packageName):
-    cmd="adb uninstall %s"%packageName
-    print getCmdEexcuteResult(cmd)
-
-def writeFile(fileName,data):
-    f=open(fileName,'a+')
-    f.write(data+"\r\n")
-    f.close
-
-def outputLog(cmd,fileName,index):
-
-        writeFile(fileName,"#########%d bigin#########\r\n"%index)
-        exeResult=getCmdEexcuteResult(cmd)
-        writeFile(fileName,"\r\n".join(exeResult))
-        writeFile(fileName,"#########%d end#########\r\n"%index)
-
-
-def meminfoLog(fileName,index):
-    cmd='adb shell dumpsys meminfo'
-    outputLog(cmd,fileName,index)
-
-def cpuinfoLog(fileName,index):
-    cmd='adb shell dumpsys cpuinfo'
-    outputLog(cmd,"fileName",index)
-
-def rank(apkNameList,index):
+def batch(apkNameList,index):
     print "under rank()"
     packageNameList=[]
     print "the %dth batch"%index
    # install 10apk
+    apkName_installed=[]
     for apkName in apkNameList:
         print "%s installing"%apkName
-        installApk(apkName)
-        writeFile("apkInstalled.log",apkName)
-    writeFile("apkInstalled.log", "************************")
+        if ((installApk(apkName)) == 0):
+            apkName_installed.append(apkName)
+    #     writeFile("apkInstalled.log",apkName)
+    # writeFile("apkInstalled.log", "************************")
 
-    # 获取包名和主界面名
-    for apkName in apkNameList:
+    # get packageName and main activity
+    for apkName in apkName_installed:
         packageName = getPackageName(apkName)
         print "packageName: " + packageName
-        writeFile("apkInstalledPackageName.log",packageName)
+        # writeFile("apkInstalledPackageName.log",packageName)
         packageNameList.append(packageName)
         packageNameListALL.append(packageName)
-    writeFile("apkInstalledPackageName.log", "************************")
+    # writeFile("apkInstalledPackageName.log", "************************")
 
-    print "loging..."
-    i = 1
-    while i <= 5:
-        meminfoLog("noStartApkMeminfo_%d.txt"%index, i)
-        cpuinfoLog("noStartApkCPUinfo_%d.txt" % index, i)
-        if i == 5:
-            break
-        time.sleep(60)  # 休眠60秒
-        i += 1
-    print "log all done"
-    print "start Apk"
-    # 启动应用
+    # print "loging..."
+    # i = 1
+    # while i <= 5:
+    #     meminfoLog("noStartApkMeminfo_%d.txt"%index, i)
+    #     cpuinfoLog("noStartApkCPUinfo_%d.txt" % index, i)
+    #     if i == 5:
+    #         break
+    #     time.sleep(60)  # 休眠60秒
+    #     i += 1
+    # print "log all done"
+    print "start Apk..........................."
+    # start APK
     for packageName in packageNameList:
         startApk(packageName)
-    print "loging..."
-    i = 1
-    while i <= 5:
-        meminfoLog("startApkMeminfo_%d.txt"%index, i)
-        cpuinfoLog("startApkCPUinfo_%d.txt" % index, i)
-        if i == 5:
-            break
-        time.sleep(60)  # 休眠60秒
-        i += 1
-    print "log all done"
-    print "all done"
+    time.sleep(30)
+    console=raw_input("delete all apk in this batch,enter y")
+    if console =='y':
+        for packageName in packageNameList:
+            uninstallApk(packageName)
+    # print "loging..."
+    # i = 1
+    # while i <= 5:
+    #     meminfoLog("startApkMeminfo_%d.txt"%index, i)
+    #     cpuinfoLog("startApkCPUinfo_%d.txt" % index, i)
+    #     if i == 5:
+    #         break
+    #     time.sleep(60)  # 休眠60秒
+    #     i += 1
+    # print "log all done"
+    # print "all done"
 
-def doWork():
-    print "under doWork()"
+
+def installAndStart():
     apkNameList=[]       # complete list
     apkNameList10N=[]        # 10 apks
-
-
-    s = os.sep
     count = 0
     for i in os.listdir(apkPath):
         if os.path.isfile(os.path.join(apkPath, i)):
@@ -143,13 +129,28 @@ def doWork():
         count+=1
         if count%10 == 0:
             print apkNameList10N
-            rank(apkNameList10N, index)            #rank every 10 apks
+            batch(apkNameList10N, index)            #rank every 10 apks
             index += 1
 
-if __name__=="__main__":
-    packageNameListALL = []
-    doWork()
-    console=raw_input("delete all apk,enter y")
-    if console =='y':
-        for packageName in packageNameListALL:
-            uninstallApk(packageName)
+
+
+# def writeFile(fileName,data):
+#     f=open(fileName,'a+')
+#     f.write(data+"\r\n")
+#     f.close
+#
+# def outputLog(cmd,fileName,index):
+#
+#         writeFile(fileName,"#########%d bigin#########\r\n"%index)
+#         exeResult=getCmdEexcuteResult(cmd)
+#         writeFile(fileName,"\r\n".join(exeResult))
+#         writeFile(fileName,"#########%d end#########\r\n"%index)
+#
+#
+# def meminfoLog(fileName,index):
+#     cmd='adb shell dumpsys meminfo'
+#     outputLog(cmd,fileName,index)
+#
+# def cpuinfoLog(fileName,index):
+#     cmd='adb shell dumpsys cpuinfo'
+#     outputLog(cmd,"fileName",index)
