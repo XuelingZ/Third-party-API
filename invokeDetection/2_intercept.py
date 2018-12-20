e0603fe7e4a5fd6f91e8a92ed0cca5a2ebfe7d26
Xueling Zhang <30911899+XuelingZ@users.noreply.github.com>
2018-12-19 23:24:58 -0600

    Update 2_intercept.py

--- invokeDetection/2_intercept.py
+++ invokeDetection/2_intercept.py
@@ -1,25 +1,77 @@
+import os
 
-import os\
-import re
-# com.flurry.android.FlurryAgent.setUserId
-flurryPath = "/home/xueling/apkAnalysis/invokeDetection/interception/flurry/"
-smaliPath = "/home/xueling/apkAnalysis/invokeDetection/smalis/"
-# for line in open(flurryPath + 'apkName_flurry').readlines():
-#     line = line.strip() + ".apk"
-#     cmd = "cp -r %s%s %s"%(smaliPath, line, flurryPath)
-#     print cmd
-#     os.system(cmd)
+apkPath = "/Users/xueling/Desktop/smalis/"
 
+APIfileName = "MixpanelAPI.smali"
+APIname = ".method public identify"
 
-
-
-#delete:
-cmd = "find . -name "MixpanelAPI.smali""
-APIfile = os.popen(cmd).readlines()
-cmd1 = "sed -i '/.method public identify/{n;d}' MixpanelAPI.smali"
-#add:
-scmd2 = "ed -i '/.method public identify/a\1111111\n2222222' MixpanelAPI.smali"
+locals_org = "locals 3"
+locals_new = "locals 4"
+targetStatement = "locals"
+text_toBeAdd = " \
+    new-instance v2, Ljava/lang/Exception;\n \
+    const-string v3, \"Xueling:printTrace with parameter UserID:\"\n \
+    invoke-direct {v2,v3}, Ljava/lang/Exception;-><init>(Ljava/lang/String;)V\n \
+    invoke-virtual {v2}, Ljava/lang/Exception;->printStackTrace()V\n \
+    invoke-static{v3, p1},Landroid/util/Log;->i(Ljava/lang/String;Ljava/lang/String;)I"
 
 
-1. find FlurryAgent.smlai
-2. find .method public static setUserId
+flag = 0
+flag_goPart2 = 0
+test_org = []
+
+text_part1 = []
+text_part2 = []
+text_new = []
+
+
+
+def generateNewAPIfile(file):
+    cmd = "find . -name \"%s\" %s" %(APIfileName, apkPath+file)
+    print cmd
+    path = os.popen(cmd).readlines()
+    print path
+    if path == "null":
+        return -1
+    APIfilePath = path[0].strip()
+    print APIfilePath
+    APIfile = open(APIfilePath)
+    text_org = APIfile.readlines()
+    print "text_org len before: %d " %len(text_org)
+    os.rename(APIfilePath, APIfilePath + "_bak")
+
+    for line in text_org:
+        if APIname in line:              # locate the API method
+            flag = 1
+
+        if flag ==1 and locals_org in line:             # change the locals
+            text_part1.append(locals_new)
+            continue
+
+        if flag == 1 and targetStatement in line:       # the place to insert
+            text_part1.append(line)
+            text_part1.append(text_toBeAdd)
+            flag = 0
+            flag_goPart2 = 1
+            continue
+
+        if flag_goPart2 == 1:
+            text_part2.append(line)
+            continue
+
+        text_part1.append(line)
+
+    APIfile.close()
+    text_new = text_part1 + text_part2
+
+    fw = open(APIfileName, 'w+')
+    for line in text_new:
+        fw.write(line + '\n')
+
+
+for file in os.listdir(apkPath):
+    if os.path.isfile(os.path.join(apkPath, file)):
+        continue
+    if generateNewAPIfile(file) == -1:
+        print "No API file found!"
+        continue
